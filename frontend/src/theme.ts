660ae1872cdfc7943044473effe1296a4931837b
Sebastien Guillemot <sebastiengllmt@gmail.com>
2023-07-08 19:40:53 +0900

    Create menu UI

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,27 +1,42 @@
 import { createTheme } from "@mui/material";
 
+declare module "@mui/material/styles" {
+  interface Palette {
+    menuButton: {
+      main: string;
+    };
+  }
+  interface PaletteOptions {
+    menuButton: {
+      main: string;
+    };
+  }
+}
 export const theme = createTheme({
   palette: {
+    menuButton: {
+      main: "#9EF79E",
+    },
     primary: {
-      main: "#F2CEAD",
-      light: "#FFE6CA",
+      main: "#73B573",
+      light: "#AFC39D",
     },
     secondary: {
-      main: "#4B230C",
+      main: "#364329",
     },
   },
   typography: {
     fontFamily: ["Inter", "Arial", "sans-serif"].join(","),
     h1: {
       fontSize: 32,
-      color: "#FFE6CA",
+      color: "#BDCEAF",
       fontWeight: 800,
     },
     button: {
       fontStyle: "normal",
       fontWeight: 700,
       fontSize: 18,
-      color: "#4B230C",
+      color: "#212919",
       textTransform: "none",
     },
   },
